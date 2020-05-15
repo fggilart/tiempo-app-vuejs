785ce4d2e8af85bc1ecdc1b09a2a31d016154ee9
Fidel Gilart <fidel.gilart@gmail.com>
2020-05-15 14:19:36 +0200

    primera versión

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,110 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <main>
+    <div class="caja-busqueda">
+      <input
+        type="text"
+        class="barra-busqueda"
+        placeholder="Buscar..."
+        v-model="consulta"
+        @keyup="obtenerTiempo"
+      />
+    </div>
+
+  <div class="tiempo-contenedor" v-if="typeof tiempo.main != 'undefined'">
+
+    <div class="caja-localizacion">
+      <div class="localizacion">{{tiempo.name}}, {{tiempo.sys.country}}</div>
+    </div>
+
+    <div class="caja-tiempo">
+      <div class="temperatura">{{  Math.round( tiempo.main.temp)  }}°C</div>
+      <div> <img v-bind:src="'http://openweathermap.org/img/wn/'
+      + tiempo.weather[0].icon +  '@2x.png'">
+      </div>
+    </div>
+
+  </div>
+
+   </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data (){
+    return{
+      api_key: '72102dd79a6ea0d4bee0a26e9c56cf28',
+      url_base: 'https://api.openweathermap.org/data/2.5/weather?q=',
+      consulta: '',
+      tiempo: {},
+    }
+  },
+  methods: {
+    obtenerTiempo (){
+       fetch(`${this.url_base}${this.consulta}&units=metric&appid=${this.api_key}`)
+       .then(function (response){
+         return response.json()
+       })
+       .then(this.establecerTiempo);
+    },
+    establecerTiempo(myJson){
+      this.tiempo = myJson;
+    }
+  },
 }
 </script>
 
 <style>
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+}
+
+body{
+  font-family: 'Rubik' , sans-serif;
+  background-color: #b6b6b6;
+  color: #404040;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  
+}
+
+main{
+  padding: 30px;
+}
+
+.caja-busqueda{
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.caja-busqueda .barra-busqueda{
+  width: 100%;
+  padding: 15px;
+  font-size: 20px;
+  color: #404040;
+  border-radius: 10px;
+  border: none;
+}
+
+.caja-localizacion .localizacion{
+  font-size: 30px;
+  font-weight: 500;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.caja-tiempo{
+  text-align: center;
+}
+
+.caja-tiempo .temperatura{
+  font-size: 90px;
+  font-weight: 700;
+  margin: 20px 0;
 }
 </style>
